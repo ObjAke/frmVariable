f457b8811f737097ff6e82af25081f0634d03cb3
VBAke <massage19830907@yahoo.co.jp>
2025-07-05 17:23:30 +0900

    プロジェクト ファイルを追加します。

---
+++ frmVariable/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace frmVariable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_Input = new System.Windows.Forms.Label();
+            this.txtBef = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAft = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtVariable = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblAlert = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(307, 38);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(132, 19);
+            this.txtEnd.TabIndex = 0;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(112, 38);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(132, 19);
+            this.txtStart.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(645, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "抽出実施";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "スタート：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "元：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(262, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "エンド：";
+            // 
+            // lbl_Input
+            // 
+            this.lbl_Input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_Input.Location = new System.Drawing.Point(38, 96);
+            this.lbl_Input.Name = "lbl_Input";
+            this.lbl_Input.Size = new System.Drawing.Size(338, 345);
+            this.lbl_Input.TabIndex = 3;
+            // 
+            // txtBef
+            // 
+            this.txtBef.Location = new System.Drawing.Point(112, 9);
+            this.txtBef.Name = "txtBef";
+            this.txtBef.Size = new System.Drawing.Size(65, 19);
+            this.txtBef.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "変数前文字：";
+            // 
+            // txtAft
+            // 
+            this.txtAft.Location = new System.Drawing.Point(288, 9);
+            this.txtAft.Name = "txtAft";
+            this.txtAft.Size = new System.Drawing.Size(65, 19);
+            this.txtAft.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(211, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "変数後文字：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(392, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 12);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "抽出後：";
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResult.Location = new System.Drawing.Point(450, 96);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(338, 345);
+            this.lblResult.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(394, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(122, 12);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "格納する変数(配列)名：";
+            // 
+            // txtVariable
+            // 
+            this.txtVariable.Location = new System.Drawing.Point(522, 9);
+            this.txtVariable.Name = "txtVariable";
+            this.txtVariable.Size = new System.Drawing.Size(132, 19);
+            this.txtVariable.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(51, 60);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(433, 12);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "スタートとエンドの中に変数化した文字を入れるときは前に　{　を、後ろに　}　をつけて下さい。";
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Location = new System.Drawing.Point(822, 5);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(1, 440);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "label9";
+            // 
+            // lblAlert
+            // 
+            this.lblAlert.AutoSize = true;
+            this.lblAlert.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblAlert.Location = new System.Drawing.Point(829, 12);
+            this.lblAlert.Name = "lblAlert";
+            this.lblAlert.Size = new System.Drawing.Size(58, 15);
+            this.lblAlert.TabIndex = 7;
+            this.lblAlert.Text = "label10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 450);
+            this.Controls.Add(this.lblAlert);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lbl_Input);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtVariable);
+            this.Controls.Add(this.txtAft);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBef);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.txtEnd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_Input;
+        private System.Windows.Forms.TextBox txtBef;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAft;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtVariable;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblAlert;
+    }
+}
+
---
+++ frmVariable/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace frmVariable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] strOriginal = lbl_Input.Text.Replace("\r\n", "\r").Split('\r');
+            //string pattern = $@"{txtBef.Text}([^>]+){txtAft.Text}"; // `txtBef.Text` と `txtAft.Text` に囲まれた文字列を取得
+
+            string pattern = $@"{txtBef.Text}(.*?){txtAft.Text}";
+
+
+            List<string> strFilters = new List<string>();   
+
+            Dictionary<string, object> dicVariable = new Dictionary<string, object>();
+
+            for (int i = 0; i < strOriginal.Length; i++)
+            {
+                //間の挟まれた数字を取得して格納
+                MatchCollection matches = Regex.Matches(strOriginal[i], pattern);
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Groups[1].Value); // 囲まれた中身を取得
+                    strFilters.Add(match.Groups[1].Value);
+                }
+            }
+
+            List<string> strStarts = new List<string>();
+            //スタートに一致するかを確認して抽出
+            if (txtStart.Text.Contains($"{{{txtVariable.Text}}}"))
+            {
+                for (int j = 0; j < strFilters.Count(); j++)
+                {
+                    strStarts.Add(txtStart.Text.Replace($"{{{txtVariable.Text}}}", strFilters[j]));
+                }
+            }
+            else
+            {
+                strStarts.Add(txtStart.Text);
+            }
+
+            List<string> strEnds = new List<string>();
+            //エンドに一致するかを確認して抽出
+            if (txtEnd.Text.Contains($"{{txtVariable}}"))
+            {
+                for (int j = 0; j < strFilters.Count(); j++)
+                {
+                    strEnds.Add(txtEnd.Text.Replace("{txtVariable}", strFilters[j]));
+                }
+            }
+            else
+            {
+                strEnds.Add(txtEnd.Text);
+            }
+
+            string strResult = string.Empty;
+            //strStartsとstrEndsが存在していたら抽出する
+            bool flgInput = false;
+            for (int i = 0; i < strOriginal.Length; i++)
+            {
+                for (int j = 0; j < strStarts.Count; j++)
+                {
+                    if (strOriginal[i].Contains(strStarts[j])) flgInput = true;
+                }
+                for (int j = 0; j < strEnds.Count; j++)
+                {
+                    if (strOriginal[i].Contains(strEnds[j]) && flgInput) flgInput = false;
+                }
+                if (flgInput)
+                {
+                    strResult += strOriginal[i] + "\r\n";
+                }
+                
+            }
+            lblResult.Text = strResult; 
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtBef.Text = @"ｲﾁ\[";
+            txtAft.Text = @": ";
+            txtVariable.Text = "iNo";
+            txtStart.Text = "ｲﾁ[{iNo}]{";
+            txtEnd.Text = "};";
+
+            lblAlert.Text =
+                "変数前文字と変数後文字に入れるときには\r\n" +
+                "必ず以下の半角文字を入れる際には右側の\r\n" +
+                "記載方法をして下さい。\r\n" +
+                @"　.　　　　　　　　→　　　\." + "\r\n" +
+                @"　^　　　　　　　　→　　　\^" + "\r\n" +
+                @"　$　　　　　　　　→　　　\$|行末の位置" + "\r\n" +
+                @"　|　　　　　　　　→　　　\|" + "\r\n" +
+                @"　\　　　　　　　　→　　　\\" + "\r\n" +
+                @"　[、または、]　　 →　　　\[, \]" + "\r\n" +
+                @"　(、または、)　　 →　　　\(, \)" + "\r\n" +
+                @"　{、または、}　　 →　　　\{, \}" + "\r\n" +
+                @"　+　　　　　　　　→　　　\+" + "\r\n" +
+                @"　*　　　　　　　　→　　　\*" + "\r\n" +
+                @"　?　　　　　　　　→　　　\?" + "\r\n";
+
+            lbl_Input.Text = 
+                "  18:ｶｸｼﾞｸ ｲﾁ[1:   300.360] 100% ﾅﾒﾗｶ50    ;\r\n" +
+                "  19:ﾁｮｸｾﾝ ｲﾁ[2:  1500.850] 2000mm/sec ﾅﾒﾗｶ50    ;\r\n" +
+                "  20:ﾁｮｸｾﾝ ｲﾁ[3:  2000.916] 2000mm/sec ﾅﾒﾗｶ50    ;\r\n" +
+                "ｲﾁ[1]{\r\n" +
+                "   GP1:\r\n" +
+                "\tUF : 0, UT : 1,\t\tｹｲﾀｲ : 'N U T, 0, 0, 0',\r\n" +
+                "\tX =   432.123  mm,\tY =   345.678  mm,\tZ =  678.543  mm,\r\n" +
+                "\tW =     1.234 deg,\tP =     1.234 deg,\tR =    23.456 deg\r\n" +
+                "   GP2:\r\n\tUF : 0, UT : 1,\t\r\n\tJ1=    23.456  mm\r\n};\r\n" +
+                "ｲﾁ[2]{\r\n" +
+                "   GP1:\r\n" +
+                "\tUF : 0, UT : 1,\t\tｹｲﾀｲ : 'N U T, 0, 0, 0',\r\n" +
+                "\tX =   789.012  mm,\tY =  345.678  mm,\tZ =  901.234  mm,\r\n" +
+                "\tW =    56.789 deg,\tP =   -12.345 deg,\tR =    678.901 deg\r\n" +
+                "   GP2:\r\n\tUF : 0, UT : 1,\t\r\n" +
+                "\tJ1=    23.456  mm\r\n" +
+                "};\r\n" +
+                "ｲﾁ[3]{\r\n" +
+                "   GP1:\r\n" +
+                "\tUF : 0, UT : 1,\t\tｹｲﾀｲ : 'N U T, 0, 0, 0',\r\n" +
+                "\tX =  1234.567  mm,\tY =  1345.267  mm,\tZ =  1456.789  mm,\r\n" +
+                "\tW =    12.345 deg,\tP =   -67.890 deg,\tR =    12.345 deg\r\n" +
+                "   GP2:\r\n\tUF : 0, UT : 1,\t\r\n" +
+                "\tJ1=    45.678  mm\r\n" +
+                "};\r\n" +
+                "ｲﾁ[4]{\r\n" +
+                "   GP1:\r\n" +
+                "\tUF : 0, UT : 1,\t\tｹｲﾀｲ : 'N U T, 0, 0, 0',\r\n" +
+                "\tX =   567.890  mm,\tY =  2345.678  mm,\tZ =  1567.890  mm,\r\n" +
+                "\tW =   -34.567 deg,\tP =   -89.012 deg,\tR =   34.567 deg\r\n" +
+                "   GP2:\r\n" +
+                "\tUF : 0, UT : 1,\t\r\n" +
+                "\tJ1=    56.789  mm\r\n" +
+                "};";
+        }
+    }
+}
